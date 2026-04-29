013bfe75f507ce2359b15cc4a3242d481baa9d76
pulmonaryveins <vincebryant42@gmail.com>
2026-04-29 16:28:32 +0800

    chore: Git-ignored env.prod

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  supabaseUrl: 'https://okftoxjvgwrpsizdyexf.supabase.co',
-  supabaseAnonKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rZnRveGp2Z3dycHNpemR5ZXhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwNjkwMzYsImV4cCI6MjA5MTY0NTAzNn0.mRU1RIHXA9jjBaufmn0BX-5ph8rlRf4kNbog3Rz1oyQ',
-};
